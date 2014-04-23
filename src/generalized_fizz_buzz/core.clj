b5cf63df39c6b477519a16eded231e60240a0ab9
Tim Destan <tim@macbookair.local>
2014-04-22 22:32:06 -0400

    Pulled out a helper function.

--- src/generalized_fizz_buzz/core.clj
+++ src/generalized_fizz_buzz/core.clj
@@ -7,17 +7,19 @@
 ;; Returns true iff the factor divides n.
 (defn divides [factor n] (= 0 (mod n factor)))
 
+;; Finds words that are associated with a multiple
+;; of n in the factors.
+(defn words-for-multiples [n factors]
+  (for [factor factors
+              :let [n' (key factor)
+                    word (val factor)]
+              :when (divides n' n)]
+          word))
+
 ;; Generates the word or number for n given the provided factors.
 (defn number-or-word [n factors]
-  (let [matchingFactors
-        (for [factor factors
-              :let [f' (key factor)
-                    word (val factor)]
-              :when (divides f' n)]
-          word)]
-    (if (empty? matchingFactors)
-      n
-      (apply str matchingFactors))))
+  (let [words (words-for-multiples n factors)]
+    (if (empty? words) n (apply str words))))
 
 ;; Generates a sequence from 1 to n, modified based on factors.
 (defn fizz-buzz [n factors]
